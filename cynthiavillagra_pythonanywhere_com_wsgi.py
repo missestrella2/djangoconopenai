e86ff8783060a161a4b3e5f99a8250074cbd432c
Cynthia V <missestrella2@gmail.com>
2025-01-01 23:40:14 -0300

    nuevo requeriments

---
+++ cynthiavillagra_pythonanywhere_com_wsgi.py
@@ -0,0 +1,17 @@
+import os
+import sys
+
+# Añade las rutas a tu proyecto
+sys.path.append('/home/cynthiavillagra/catering_V2/catering_V2')  # Ruta base del proyecto
+sys.path.append('/home/cynthiavillagra/catering_V2/catering_V2/catering_V2')  # Ruta donde está settings.py
+
+# Activa el entorno virtual
+activate_this = '/home/cynthiavillagra/.virtualenvs/myvirtualenv/bin/activate_this.py'
+exec(open(activate_this).read(), {'__file__': activate_this})
+
+# Configura DJANGO_SETTINGS_MODULE para que apunte correctamente al archivo settings.py
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'catering_V2.settings')
+
+# Importa y configura la aplicación WSGI
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
